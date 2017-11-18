6e9e23e623cc353964f56907934bda0c93855f1c
MMil05 <maksymilian.milkowski@gmail.com>
2017-11-18 19:27:39 +0100

    FindUserById servlet

---
+++ src/main/java/com/infoshareacademy/searchengine/servlets/WelcomeUserServlet.java
@@ -0,0 +1,26 @@
+package com.infoshareacademy.searchengine.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/welcome-user")
+public class WelcomeUserServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        if (req.getParameter("name") == null) {
+            // resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
+            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
+            PrintWriter writer = resp.getWriter();
+            writer.println("<!DOCTYPE html><html><body> czesc: "+req.getParameter("name")+"</body></html>");
+
+    }
+}
