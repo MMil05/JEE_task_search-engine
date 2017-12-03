c33ff3f39dfb5d8c8969eb72f336e83065563e62
MMil05 <maksymilian.milkowski@gmail.com>
2017-12-03 18:22:38 +0100

    Hibernate

--- src/main/java/com/infoshareacademy/searchengine/dao/UsersRepositoryDaoBean.java
+++ src/main/java/com/infoshareacademy/searchengine/dao/UsersRepositoryDaoBean.java
@@ -4,11 +4,16 @@ import com.infoshareacademy.searchengine.domain.User;
 import com.infoshareacademy.searchengine.repository.UsersRepository;
 
 import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.ArrayList;
 import java.util.List;
 
 @Stateless
 public class UsersRepositoryDaoBean implements UsersRepositoryDao, UsersRepositoryDaoRemote {
+
+    @PersistenceContext(unitName = "pUnit")
+    private EntityManager entityManager;
 
     public void addUser(User user) {
         UsersRepository.getRepository().add(user);
